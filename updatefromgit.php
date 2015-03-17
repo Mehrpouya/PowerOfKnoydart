c2ea91e3567ab65af8814f76dfee3a1d96e390f6
Mehrpouya <h.mehrpooya@gmail.com>
2015-03-17 12:57:04 +0000

    Update updatefromgit.php

--- updatefromgit.php
+++ updatefromgit.php
@@ -1,10 +1,3 @@
 <?php <?php
-
-define(‘PRIVATE_KEY’, ‘F37Yb*@AD9YkQx4’);
-
-if ($_SERVER[‘REQUEST_METHOD’] === ‘POST’    
-        && $_REQUEST['thing'] === PRIVATE_KEY)
-{    
     echo shell_exec("git pull");
-}
 ?>
